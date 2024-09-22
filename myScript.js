73c0f5fe777b4b828af556689f547755c818c0ba
EshaanJoshiSDBI <eshaanjoshi713@gmail.com>
2024-09-22 19:55:29 +0530

    new design

---
+++ myScript.js
@@ -0,0 +1,9 @@
+// JavaScript to toggle the hamburger menu
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById('hamburger');
+    const navList = document.getElementById('nav-list');
+
+    hamburger.addEventListener('click', function() {
+        navList.classList.toggle('show');
+    });
+});
